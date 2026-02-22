d559241d81b646e6300c2b8bfb6cd30eb476c211
Yoann <rabahiyoann@gmail.com>
2026-02-22 16:04:17 +0100

    supression de l'import inutile

--- mini-projet-app/src/App.tsx
+++ mini-projet-app/src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import Header from "./components/Header/Header";
-import ProteinTable from "./components/ProteinTable/ProteinTable";
 import ProteinForm from "./components/ProteinForm/ProteinForm";
 
 function App() {
