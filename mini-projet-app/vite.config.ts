5385529e893bc6c7bd01c3c3fe41cb6babfed0ed
Yoann <rabahiyoann@gmail.com>
2026-02-25 19:05:28 +0100

    commit

--- mini-projet-app/vite.config.ts
+++ mini-projet-app/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // permet d'écouter sur 0.0.0.0
+  }
 })
